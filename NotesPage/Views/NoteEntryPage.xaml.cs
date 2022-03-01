14b2559d7748633c837bb218d6157799841d1b3c
Samir Kumar <sam2elect@gmail.com>
2022-03-02 05:10:21 +0530

    Add project files.

---
+++ NotesPage/Views/NoteEntryPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿async void LoadNote(string itemId)
+{
+    try
+    {
+        int id = Convert.ToInt32(itemId);
+        // Retrieve the note and set it as the BindingContext of the page.
+        Note note = await App.Database.GetNoteAsync(id);
+        BindingContext = note;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Failed to load note.");
+    }
+}
+
+async void OnSaveButtonClicked(object sender, EventArgs e)
+{
+    var note = (Note)BindingContext;
+    note.Date = DateTime.UtcNow;
+    if (!string.IsNullOrWhiteSpace(note.Text))
+    {
+        await App.Database.SaveNoteAsync(note);
+    }
+
+    // Navigate backwards
+    await Shell.Current.GoToAsync("..");
+}
+
+async void OnDeleteButtonClicked(object sender, EventArgs e)
+{
+    var note = (Note)BindingContext;
+    await App.Database.DeleteNoteAsync(note);
+
+    // Navigate backwards
+    await Shell.Current.GoToAsync("..");
+}
---
+++ NotesPage/Views/NotesPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿protected override async void OnAppearing()
+{
+    base.OnAppearing();
+
+    // Retrieve all the notes from the database, and set them as the
+    // data source for the CollectionView.
+    collectionView.ItemsSource = await App.Database.GetNotesAsync();
+}
+
+async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+{
+    if (e.CurrentSelection != null)
+    {
+        // Navigate to the NoteEntryPage, passing the ID as a query parameter.
+        Note note = (Note)e.CurrentSelection.FirstOrDefault();
+        await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.ID.ToString()}");
+    }
+}
